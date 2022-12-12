f685fb7120f32ccc40b6ef59cd74994f39e0d25b
Andy Beers <andybeers@gmail.com>
2022-12-11 21:38:51 -0800

    Day 3 part 2 solution

--- 2022/03/index.ts
+++ 2022/03/index.ts
@@ -12,8 +12,17 @@ for (let i = 1; i < 27; i++) {
   priorityMap[uppercaseChar] = 26 + i
 }
 
-function findCommonElement(arr1: string[], arr2: string[]) {
-  return arr1.find((el1) => arr2.find((el2) => el2 === el1))
+function findCommonElement(arr1: string[], arr2: string[], arr3?: string[]) {
+  return arr3
+    ? arr1.find((el1) =>
+        arr2.find((el2) => {
+          if (el2 !== el1) {
+            return false
+          }
+          return arr3.find((el3) => el3 === el2)
+        })
+      )
+    : arr1.find((el1) => arr2.find((el2) => el2 === el1))
 }
 
 function gatherMisplacedElements(bags: string[]) {
@@ -38,3 +47,25 @@ function getPriorityTotal() {
 }
 
 console.log(getPriorityTotal())
+
+function getBadgePriorityTotal() {
+  let total = 0
+  let startIndex = 0
+
+  for (let i = 0; i < bags.length / 3; i++) {
+    const commonItem = findCommonElement(
+      bags[startIndex].split(''),
+      bags[startIndex + 1].split(''),
+      bags[startIndex + 2].split('')
+    )
+    startIndex += 3
+    if (!commonItem) {
+      return
+    }
+    total += priorityMap[commonItem]
+  }
+
+  return total
+}
+
+console.log(getBadgePriorityTotal())
